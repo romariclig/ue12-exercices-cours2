3acec114fc23860cd201f92494950ffe988e052c
romariclig <romaric.ligeret@etu.minesparis.psl.eu>
2024-10-01 15:01:14 +0200

    peu

--- exercices.py
+++ exercices.py
@@ -13,7 +13,7 @@ import numpy as np
 
 # %%
 def create_zeros():
-    pass  # 👈 Insérez le code ici
+    return np.zeros(5)  # 👈 Insérez le code ici
 
 
 print(create_zeros())  # attendu: [0. 0. 0. 0. 0.]
@@ -26,7 +26,7 @@ print(create_zeros())  # attendu: [0. 0. 0. 0. 0.]
 
 # %%
 def create_ones():
-    pass  # 👈 Insérez le code ici
+    return np.ones(5)  # 👈 Insérez le code ici
 
 
 print(create_ones())  # attendu: [1. 1. 1. 1. 1.]
@@ -39,7 +39,7 @@ print(create_ones())  # attendu: [1. 1. 1. 1. 1.]
 
 # %%
 def create_range():
-    pass  # 👈 Insérez le code ici
+    return np.arange(10,51)  # 👈 Insérez le code ici
 
 
 print(create_range())  # attendu: [10 11 12 13 14 15 16 17 ...] (jusqu'à 50 inclus)
@@ -52,7 +52,7 @@ print(create_range())  # attendu: [10 11 12 13 14 15 16 17 ...] (jusqu'à 50 inc
 
 # %%
 def create_identity():
-    pass  # 👈 Insérez le code ici
+    return np.array([[1.,0,0],[0,1.,0],[0,0,1.]])  # 👈 Insérez le code ici
 
 
 print(create_identity())  # attendu: [[1. 0. 0.]
@@ -67,7 +67,7 @@ print(create_identity())  # attendu: [[1. 0. 0.]
 
 # %%
 def create_random():
-    pass  # 👈 Insérez le code ici
+    return np.random.randn(3,3)  # 👈 Insérez le code ici
 
 
 print(create_random())  # attendu: un tableau 3x3 de valeurs aléatoires entre 0 et 1
@@ -83,7 +83,7 @@ print(create_random())  # attendu: un tableau 3x3 de valeurs aléatoires entre 0
 
 # %%
 def add_five(arr):
-    pass  # 👈 Insérez le code ici
+    return np.add(arr,5*np.ones(arr.shape, dtype=np.uint8)) # 👈 Insérez le code ici
 
 
 print(add_five(np.array([1, 2, 3, 4, 5])))  # attendu: [6 7 8 9 10]
@@ -96,7 +96,7 @@ print(add_five(np.array([1, 2, 3, 4, 5])))  # attendu: [6 7 8 9 10]
 
 # %%
 def square(arr):
-    pass  # 👈 Insérez le code ici
+    return np.multiply(arr,arr)  # 👈 Insérez le code ici
 
 
 print(square(np.array([1, 2, 3, 4, 5])))  # attendu: [ 1  4  9 16 25]
@@ -109,7 +109,8 @@ print(square(np.array([1, 2, 3, 4, 5])))  # attendu: [ 1  4  9 16 25]
 
 # %%
 def sin_values():
-    pass  # 👈 Insérez le code
+    tab=np.arange(0,2*np.pi,0.1)
+    return np.sin(tab)  # 👈 Insérez le code
 
 
 print(
@@ -132,7 +133,7 @@ def f(arr1, arr2):
 
 
 def f_vectorized(arr1, arr2):
-    pass  # 👈 Insérez le code ici
+    return np.add(2*arr1,3*arr2)  # 👈 Insérez le code ici
 
 
 print(
@@ -157,7 +158,9 @@ def g(x):
 
 
 def g_vectorized(x):
-    pass  # 👈 Insérez le code ici
+    tab1=np.copy(x)
+    x[x<=0]=1
+    return np.multiply(tab1,x)  # 👈 Insérez le code ici
 
 
 print(g_vectorized(np.array([1, -2, 3, -4, 5])))  # attendu: [ 1 -2  9 -4 25]
